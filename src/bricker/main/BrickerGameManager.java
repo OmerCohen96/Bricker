694c86a5c46c7b2ecaeab2bbd157b67d6d923810
OmerCohen96 <Omeriko175@gmail.com>
2023-08-02 21:01:46 +0300

    extending BrickerGameManeger with GameManager

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -3,10 +3,16 @@ package bricker.main;
 import danogl.GameManager;
 import danogl.util.Vector2;
 
-public class BrickerGameManager {
+public class BrickerGameManager extends GameManager {
+
+    public BrickerGameManager (String windowTitle, Vector2 windowsDimension){
+        super(windowTitle, windowsDimension);
+    }
     public static void main(String[] args) {
-        new GameManager("Bricker",
-                new Vector2(700, 500)).run();
+        new BrickerGameManager("Bricker", new Vector2(700, 500)).run();
+
+//        new GameManager("Bricker",
+//                new Vector2(700, 500)).run();
 
 
     }
